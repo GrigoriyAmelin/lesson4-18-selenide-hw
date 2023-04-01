c02e711a6ce4e01a70d6a7e12cc73c2a03eafb50
Grigoriy Amelin <g-amelin@mail.ru>
2023-04-01 21:17:30 +0300

    added Test shouldFindSelenideRepositoryAtTheTop

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,4 +1,33 @@
 package github;
 
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class SelenideRepositorySearch {
+
+    @Test
+    void shouldFindSelenideRepositoryAtTheTop () {
+
+        // open main page
+        open("https://github.com");
+        // enter into search field "selenide" and press ENTER
+        $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
+        // click on the first repository at the list of found repositories
+        $$("ul.repo-list li").first().$("a").click();
+        // check that header is "selenide/selenide"
+        $("#repository-container-header").shouldHave(text("selenide /\nselenide"));
+        sleep(5000);
+
+        // ARRANGE
+
+        // ACT
+
+        // ASSERT
+
+
+
+    }
 }
