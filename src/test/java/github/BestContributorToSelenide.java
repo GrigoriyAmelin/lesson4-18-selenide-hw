2e091b2ba775ebb555130c920f040bd8625d42c0
Grigoriy Amelin <g-amelin@mail.ru>
2023-04-02 22:19:33 +0300

    added BestContributorToSelenide.java

---
+++ src/test/java/github/BestContributorToSelenide.java
@@ -0,0 +1,27 @@
+package github;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BestContributorToSelenide {
+
+    @Test
+    void andreiSolntcevShouldBeTheFirstContributor () {
+
+        // open page with repository of selenide
+        open("https://github.com/selenide/selenide");
+        // move a mouse to the first avatar in a block
+        $("div.Layout-sidebar").$(byText("Contributors")).closest(".BorderGrid-cell").
+                $$("ul li").first().hover();
+        // check that pop-up window include the text "Andrei Solntsev"
+        $$(".Popover").findBy(visible).shouldHave(text("Andrei Solntsev"));
+
+        sleep(5000);
+    }
+}
